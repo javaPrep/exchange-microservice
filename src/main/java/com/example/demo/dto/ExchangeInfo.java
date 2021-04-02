9769f37801e5efd34c16a4fdd38346ca2861c1d1
Next <Next@Admin>
2021-04-02 18:47:35 +0530

    initial commit

---
+++ src/main/java/com/example/demo/dto/ExchangeInfo.java
@@ -0,0 +1,17 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExchangeInfo {
+	private String currency;
+
+}
